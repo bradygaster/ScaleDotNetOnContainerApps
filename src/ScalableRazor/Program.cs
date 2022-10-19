c2a8230db68c3254b937ceee5d25f679a4f84e8c
Brady Gaster <bradyg@microsoft.com>
2022-10-18 20:09:44 -0700

    works when scaled

--- src/ScalableRazor/Program.cs
+++ src/ScalableRazor/Program.cs
@@ -1,8 +1,19 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Azure;
+
 var builder = WebApplication.CreateBuilder(args);
+var BlobStorageUri = builder.Configuration["AzureURIs:BlobStorage"];
+var KeyVaultURI = builder.Configuration["AzureURIs:KeyVault"];
+var cred = new DefaultAzureCredential();
 
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddAzureClientsCore();
+builder.Services.AddDataProtection()
+                .PersistKeysToAzureBlobStorage(new Uri(BlobStorageUri), cred)
+                .ProtectKeysWithAzureKeyVault(new Uri(KeyVaultURI), cred);
 
 var app = builder.Build();
 
