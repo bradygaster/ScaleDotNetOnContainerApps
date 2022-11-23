460a3dfbcbd8b7f839bfb4222f7c2a9bc70821fb
Brady Gaster <bradyg@microsoft.com>
2022-11-23 14:15:02 -0800

    Added deliberate error page to demonstrate how the devloper exception page captures normal app exceptions.

---
+++ src/ScalableRazor/Pages/Throw.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScalableRazor.Pages
+{
+    public class ThrowModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
