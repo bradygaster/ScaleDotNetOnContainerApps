3bba942d11dfc7ee1db15e5274915f9d8e78b1d1
Brady Gaster <bradyg@microsoft.com>
2023-01-03 14:14:44 -0800

    working with simple middleware to wire it all up at once.

---
+++ src/ScalableRazor/DistributeWithDefaultsExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace ScalableRazor
+{
+    public static class DistributeWithDefaultsExtensions
+    {
+        public static WebApplicationBuilder AddDistributedDefaults(this WebApplicationBuilder builder)
+        {
+            var storageConnectionString = string.IsNullOrEmpty(builder.Configuration.GetValue<string>("AZURE_STORAGE_CONNECTION_STRING"))
+                                        ? "UseDevelopmentStorage=true"
+                                        : builder.Configuration.GetValue<string>("AZURE_STORAGE_CONNECTION_STRING");
+
+            builder.Services.AddOrleans(siloBuilder =>
+            {
+                siloBuilder
+                    .UseAzureStorageClustering(azureStorageConfigurationOptions =>
+                    {
+                        azureStorageConfigurationOptions.ConfigureTableServiceClient(storageConnectionString);
+                    })
+                    .AddAzureTableGrainStorageAsDefault(azureBlobGrainStorageOptions =>
+                    {
+                        azureBlobGrainStorageOptions.ConfigureTableServiceClient(storageConnectionString);
+                    });
+            });
+
+            builder.Services.AddOrleansDistributedCache(options =>
+            {
+                options.PersistWhenSet = true;
+                options.DefaultDelayDeactivation = TimeSpan.FromMinutes(5);
+            });
+
+            builder.Services.AddDataProtection()
+                            .PersistKeysToOrleans();
+
+            return builder;
+        }
+    }
+}
--- src/ScalableRazor/Program.cs
+++ src/ScalableRazor/Program.cs
@@ -1,57 +1,18 @@
-using Azure.Identity;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.Extensions.Azure;
 using ScalableRazor;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var storageConnectionString = string.IsNullOrEmpty(builder.Configuration.GetValue<string>("AZURE_STORAGE_CONNECTION_STRING"))
-    ? "UseDevelopmentStorage=true"
-    : builder.Configuration.GetValue<string>("AZURE_STORAGE_CONNECTION_STRING");
 
 builder.Services.AddRazorPages();
 builder.Services.AddHttpClient();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSession();
+
+// application services
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddSingleton<FavoritesService>();
+builder.Services.AddScoped<FavoritesService>();
 
-builder.Services.AddOrleans(siloBuilder =>
-{
-    siloBuilder
-        .UseAzureStorageClustering(azureStorageConfigurationOptions =>
-        {
-            azureStorageConfigurationOptions.ConfigureTableServiceClient(storageConnectionString);
-        })
-        .AddAzureTableGrainStorageAsDefault(azureBlobGrainStorageOptions =>
-        {
-            azureBlobGrainStorageOptions.ConfigureTableServiceClient(storageConnectionString);
-        })
-        //.ConfigureServices(services =>
-        //{
-        //    var BlobStorageUri = builder.Configuration["AzureURIs:BlobStorage"];
-        //    var KeyVaultURI = builder.Configuration["AzureURIs:KeyVault"];
-        //    var azureCredential = new DefaultAzureCredential();
-        //    builder.Services.AddAzureClientsCore();
-        //    builder.Services.AddDataProtection()
-        //                    .PersistKeysToAzureBlobStorage(new Uri(BlobStorageUri), azureCredential)
-        //                    .ProtectKeysWithAzureKeyVault(new Uri(KeyVaultURI), azureCredential)
-        //                    ;
-            
-        //})
-        ;
-});
-
-
-
-builder.Services.AddOrleansDistributedCache(options =>
-{
-    options.PersistWhenSet = true;
-    options.DefaultDelayDeactivation = TimeSpan.FromMinutes(5);
-});
-
-builder.Services.AddDataProtection()
-                .PersistKeysToOrleans();
+// enable distributed processing and scale-out
+builder.AddDistributedDefaults();
 
 var app = builder.Build();
 
