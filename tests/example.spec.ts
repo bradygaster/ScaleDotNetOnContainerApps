7e8bbee7dd24af62133fce5a4d0a1ff7353398e1
Brady Gaster <bradyg@microsoft.com>
2023-01-05 23:50:47 -0800

    added UX tests

---
+++ tests/example.spec.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@playwright/test';
+
+let siteUrl = 'https://githubsearchrazorapp.greendune-def3b6f9.eastus.azurecontainerapps.io/';
+let organizations: string[] = ['microsoft', 'azure', 'github', 'netflix', 'twitter', 'facepunch', 'dotnet', 'apache', 'ibm', 'openapi', 'facebook'];
+
+test('has title', async ({ page }) => {
+  await page.goto(siteUrl);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/GitHub Browser/);
+});
+
+test('can search for repos', async ({ page }) => {
+  await page.goto(siteUrl);
+
+  // Get a random organization name
+  var rnd = Math.floor(Math.random() * organizations.length);
+  var searchTerm = organizations[rnd];
+
+  // Fill an input.
+  console.log("Searching for " + searchTerm);
+  await page.locator('#searchTerm').fill(searchTerm);
+
+  // Click the search button.
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.waitForLoadState();
+
+  //  Make sure the search text was persisted
+  var searchBox = await page.locator('#searchTerm');
+  await expect(searchBox).toHaveValue(searchTerm);
+
+  // Make sure the search is included in the list of recent searches
+  var recentSearchButton = await page.getByRole('button', { name: searchTerm });
+  await expect(await recentSearchButton.count()).toBeGreaterThan(0);
+
+  // Make sure the search results are visible
+  var favoriteButtons = await page.getByRole('button', { name: 'Favorite' });
+  await expect(await favoriteButtons.count()).toBeGreaterThan(0);
+});
+
+test('can see recent searches', async ({ page }) => {
+  await page.goto(siteUrl);
+  var randomOrgs = organizations.sort(() => Math.random() - Math.random()).slice(0, 3);
+  for (const org of randomOrgs) {
+    // Fill an input.
+    console.log("Searching for " + org);
+    await page.locator('#searchTerm').fill(org);
+
+    // Click the search button.
+    await page.getByRole('button', { name: 'Search' }).click();
+    await page.waitForLoadState();
+
+    //  Make sure the search text was persisted
+    var searchBox = await page.locator('#searchTerm');
+    await expect(searchBox).toHaveValue(org);
+
+    // Make sure the search is included in the list of recent searches
+    var recentSearchButton = await page.getByRole('button', { name: org });
+    await expect(await recentSearchButton.count()).toBeGreaterThan(0);
+
+    // Make sure the search results are visible
+    var favoriteButtons = await page.getByRole('button', { name: 'Favorite' });
+    await expect(await favoriteButtons.count()).toBeGreaterThan(0);
+  }
+
+  // Make sure the recent searches are visible
+  for (const org of randomOrgs) {
+    console.log("Validating " + org + " is still here.");
+    var recentSearchButton = await page.getByRole('button', { name: org });
+    await expect(await recentSearchButton.count()).toBeGreaterThan(0);
+  }
+});
